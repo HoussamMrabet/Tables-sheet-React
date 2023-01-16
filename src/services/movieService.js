a97444b1376226bb221c4a164bd16731ff2d7437
Houssam Mrabet <dodslim100@gmail.com>
2023-01-16 04:05:10 +0100

    deleting temporary data files and refactoring some codes

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,25 +3,29 @@ import config from '../config.json';
 
 const moviesAPI = config.apiUrl + '/movies';
 
+function movieUrl(id){
+  return `${moviesAPI}/${id}`;
+}
+
 export function getMovies() {
     return http.get(moviesAPI);
 }
 
 export function getMovie(id) {
-  return http.get(moviesAPI + '/' + id);
+  return http.get(movieUrl(id));
 }
 
 export async function saveMovie(movie) {
   if(movie._id){
     let body = {...movie};
     delete body._id;
-    return http.put(moviesAPI + '/' + movie._id, body);
+    return http.put(movieUrl(movie._id), body);
   }
 
   return http.post(moviesAPI, movie);
 }
 
 export async function deleteMovie(id) {
-  return http.delete(moviesAPI+'/'+id);
+  return http.delete(movieUrl(id));
 }
   
